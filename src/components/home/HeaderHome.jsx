3d545145b8eb086b79ecd9f567a6b9760d682449
Thiago Rocha <thiagorocha841@gmail.com>
2026-09-03 00:31:53 -0300

    fix subtitle headerHome

--- src/components/home/HeaderHome.jsx
+++ src/components/home/HeaderHome.jsx
@@ -105,7 +105,7 @@ export default function Header() {
               >
                 <li>
                   <Link href="/branding" onClick={closeAll}>
-                    Branding L
+                    Branding
                   </Link>
                 </li>
                 <li>
